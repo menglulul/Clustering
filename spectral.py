ffef1097d90502bd5921194ddc200e9683ca7402
SherryXiaoyingLi <xl2gs@virginia.edu>
2019-10-08 22:15:04 -0400

    spectral fix

--- spectral.py
+++ spectral.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
-import math
 from sklearn.cluster import KMeans, SpectralClustering
+# import math
 
+# def gaussianKernel(a, b):
+#     sigma = 10
+#     euclidean = np.linalg.norm(a-b)
+#     res = math.exp(-0.5 * (euclidean ** 2) / (sigma ** 2 ) )
+#     return res
 
-def gaussianKernel(a, b):
-    sigma = 10
-    euclidean = np.linalg.norm(a-b)
-    res = math.exp(-0.5 * (euclidean ** 2) / (sigma ** 2 ) )
-    return res
-
-
-def laplacianFully(df):
+def laplacianFully(df, k):
     num_rows = df.shape[0]
-    A = np.zeros((num_rows, num_rows))
-    D = np.zeros((num_rows, num_rows))
+    W = np.zeros((num_rows, num_rows))
     for i in range(num_rows):
         a = df.iloc[i, :]
         for j in range(i+1, num_rows):
             b = df.iloc[j, :]
-            # use gaussian function for similarity measure
-            A[i][j] = gaussianKernel(a, b)
-            A[j][i] = A[i][j]
-        D[i][i] = np.sum(A[i])
+            W[i][j] = np.linalg.norm(a-b)
+            W[j][i] = W[i][j]
+
+    # use gaussian kernel for similarity function
+
+    # calculate a matrix of variance measure for each individual dist in W (local scaling)
+    # get the d(si, sk) -- single row at row k of sorted W
+    # (kth smallest dist in all rows/objects for each attribute)
+    ksub = np.sort(W, axis=0)
+    ksub = ksub[k]
+    # print("ksub", ksub)
+    # calculate sigma squared using matrix multiplication
+    ksub = ksub[np.newaxis].T
+    sigma_square = np.multiply(ksub, ksub.T)
+
+    A = np.exp(-1 * W * W / sigma_square)
+
+    # calculate degree matrix
+    D = np.zeros((num_rows, num_rows))
+    rowsums = np.sum(A, axis=1).tolist()
+    for q in range(num_rows):
+        D[q][q] = rowsums[q]
+
     return D, A
 
 def laplacianEpsilon(df, epsilon):
@@ -36,7 +52,6 @@ def laplacianEpsilon(df, epsilon):
             b = df.iloc[j, :]
             # use euclidean function for similarity measure
             euclidean = np.linalg.norm(a - b)
-            #gaussian = gaussianKernel(a, b)
             if euclidean >= epsilon:
                 A[i][j] = euclidean
                 A[j][i] = A[i][j]
@@ -60,8 +75,6 @@ def sklearnKmeans(V, k):
     kmeans = KMeans(n_clusters=k, init='random', random_state= 0 ).fit(V)
     clusters = kmeans.labels_
     centroids = kmeans.cluster_centers_
-    print(clusters)
-    print(centroids)
     return clusters, centroids
 
 # the following is for comparison b/w sklearn and our self implemented spectral clustering
@@ -73,10 +86,10 @@ def sklearnSpectral(dataset, k):
 
 def spectral_cluster(dataset, k):
 
-    D, A = laplacianFully(dataset)
+    D, A = laplacianFully(dataset, k)
     #D, A = laplacianEpsilon(dataset, 1.5)
+
     L = np.subtract(D, A)
-    # Lnorm = np.linalg.inv(D) * L
     print(A)
     print(D)
     print(L)
